248432f2884fb3278f097386df950251f079bc10
d0niek <damianglinkowski@gmail.com>
2016-03-20 16:55:28 +0100

    Przeszukiwanie grafu wszerz - bfs

--- Grapth.h
+++ Grapth.h
@@ -31,7 +31,7 @@ class Grapth
          * @param int e
          * @param E d
          */
-        void edgeD(int b, int e, E d)
+        void edgeD(int b, int e, E d = E())
         {
             g[b].PB(Edge<E>(d, e));
         }
@@ -43,10 +43,54 @@ class Grapth
          * @param int e
          * @param E d
          */
-        void edgeU(int b, int e, E d)
+        void edgeU(int b, int e, E d = E())
         {
             g[b].PB(Edge<E>(d, e));
             g[e].PB(Edge<E>(d, b));
+        }
+
+        /**
+         * Przeszukiwanie grafu wszerz.
+         *
+         * Wierzchołek musi zawierać dodatkowe informacje:
+         * int t - odległość wierzchołka od źródła wyszukiwania
+         * int s - nr wierzchołka, z którego prowadzi znaleziona, najkrótsza trasa
+         *
+         * @param s int
+         */
+        void bfs(int s)
+        {
+            for (auto it = g.begin(); it != g.end(); it++) {
+                it->setT(-1);
+                it->setS(-1);
+            }
+
+            g[s].setT(0);
+
+            int qu[SIZE(g)], b, e;
+
+            qu[b = e = 0] = s;
+
+            while (b <= e) {
+                s = qu[b++];
+
+                for (auto it = g[s].begin(); it != g[s].end(); it++) {
+                    if (g[it->getV()].getT() == -1) {
+                        g[qu[++e] = it->getV()].setT(g[s].getT() + 1);
+                        g[it->getV()].setS(s);
+                    }
+                }
+            }
+        }
+
+        /**
+         * Pobierz g
+         *
+         * @return std::vector<Vertex<V, E>>
+         */
+        std::vector<Vertex<V, E> > getG()
+        {
+            return this->g;
         }
 };
 
